0c63c45793a6465c5f39c42ba5db31e11e6df7d3
minadanems-bit <mina.da.nems@gmail.com>
2026-02-27 14:38:20 +0400

    Update database.py

--- database.py
+++ database.py
@@ -11,8 +11,19 @@ import threading
 # SAFE DATABASE LOCATION
 # =====================================================
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_FILE = os.path.join(PROJECT_DIR, "nms_system.db")
+import os
+
+# ✅ Cloud Safe Storage
+if os.path.exists("/mount"):
+    BASE_DIR = "/mount/data"
+else:
+    BASE_DIR = os.path.join(os.getcwd(), "data")
+
+os.makedirs(BASE_DIR, exist_ok=True)
+
+DB_FILE = os.path.join(BASE_DIR, "nms_system.db")
+
+print("📂 DATABASE FILE:", DB_FILE)
 
 # Lock لحماية الكتابة من التداخل
 db_lock = threading.Lock()
