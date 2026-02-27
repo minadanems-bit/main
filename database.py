a0d179713828c47c33b094b107a3b60afc252f16
minadanems-bit <mina.da.nems@gmail.com>
2026-02-27 14:03:47 +0400

    Update database.py

--- database.py
+++ database.py
@@ -91,7 +91,6 @@ def save_db(data):
     conn.commit()
     conn.close()
 
-print("DATABASE INITIALIZED")
 # =====================================================
 # GET MANAGER PHONE
 # =====================================================
