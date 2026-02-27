3a279e22a537fda7febedeb9c4d59844263a37fc
minadanems-bit <mina.da.nems@gmail.com>
2026-02-27 09:26:08 +0400

    Update app.py

--- app.py
+++ app.py
@@ -771,23 +771,23 @@ with c_o3:
             "Jam": jam
         }
 
-        st.session_state["printer_start"] = printer_start
+    st.session_state["printer_start"] = printer_start
     
-        st.divider()
+    st.divider()
     
-        ops = st.number_input(
-            "Opay Start Balance",
-            step=0.01,
-            key="ops",
-            on_change=sync_draft
-        )
-    
-        u10 = st.number_input(
-            "Debit",
-            step=1.0,
-            key="u10_val",
-            on_change=sync_draft
-        )
+    ops = st.number_input(
+        "Opay Start Balance",
+        step=0.01,
+        key="ops",
+        on_change=sync_draft
+    )
+
+    u10 = st.number_input(
+        "Debit",
+        step=1.0,
+        key="u10_val",
+        on_change=sync_draft
+    )
         
 
 # --- TAB 2: CLOSING ---
